cf7d2ce56fffe19e1a9e9c7b756014a6adc944a4
古林大輝 <daiki-h9.2.14@i.softbank.jp>
2018-10-27 22:26:53 +0900

    UnityChan3Dゲーム作品です。

---
+++ Assets/PrefabDestoroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabDestoroy : MonoBehaviour {
+
+	public GameObject unitychan;
+
+	// Use this for initialization
+	void Start () {
+		unitychan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (unitychan.transform.position.z > this.transform.position.z){
+			GameObject.Destroy(this.gameObject);
+		}
+		
+	}
+}
